03b6c9055ad6628f559bc24dee8d1ad710d99f5e
AC287 <arthurchen287@gmail.com>
2016-06-16 20:02:09 -0400

    testing with bootstrap

---
+++ js/touchbox.js
@@ -0,0 +1,15 @@
+// (function($){
+//   $
+//   console.log("touchbox.js loaded");
+// })(jQuery);
+$(function(){
+  console.log('loaded......');
+  $('.dialog').hide();
+  $('.time.checkbox input').on("click",function(event){
+    // event.stopPropagation();
+    // event.preventDefault();
+    console.log('input clicked.....')
+    $('.dialog').show();
+    console.log($(this)[0].id);
+  })
+});
