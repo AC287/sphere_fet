692158e155999fd08f72055fd957ff29ab522ac9
AC287 <arthurchen287@gmail.com>
2016-06-20 00:34:35 -0400

    initial value set for menubox.

--- js/touchbox.js
+++ js/touchbox.js
@@ -18,10 +18,23 @@ $(function(){
       modal: true
     });
     $('.time.checkbox input').click(function(){
+      // splitfunction($(this)[0].id);
+      var initialTime = splitfunction($(this)[0].id);
+      console.log(initialTime);
+      // console.log(typeof initialTime[0]);
+      // console.log(typeof initialTime[1]);
+      // console.log(typeof initialTime[2]);
+      // $('#startDateForm').attr('value',initialTime[0]); //this doesn't work.
+
+      $('#startDateForm').val(initialTime[0]);
+      $('#endDateForm').val(initialTime[0]);
+
+      $('#startTimeForm').val(initialTime[1]);
+      $('#endTimeForm').val(initialTime[1]);
+
+      $('#startAmPmForm').val(initialTime[2]);
+      $('#endAmPmForm').val(initialTime[2]);
       $('.menubox').dialog('open');
-      splitfunction($(this)[0].id)
-      // var initialTime = splitfunction($(this)[0].id);
-      // console.log(initialTime);
     });
   });
 
@@ -31,6 +44,11 @@ $(function(){
   // })
 });
 
-function splitfunction(x) {
+function splitfunction(x) { //this will split input value checkbox.
   console.log(x);
+  var day = x.substr(0,3);
+  var time = x.substring(3,(x.length-2));
+  var ampm = x.substr(-2,2);
+  return [day,time,ampm]
+
 }
